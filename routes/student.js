3899c4f286d346c0e992c925389a8adb6dfd18d6
niigyanchristian <niigyanchristian@gmail.com>
2023-01-23 01:15:15 +0000

    version 1.0.5

--- routes/student.js
+++ routes/student.js
@@ -4,7 +4,6 @@ const User = require('../models/user');
 const Mock = require('../models/mock');
 const Student = require('../models/student');
 const jwt = require('jsonwebtoken');
-const { getAggregate } = require('../aggregate');
 const { calcAggregate } = require('../functions/calcAggregate');
 const router = express.Router();
 // const aggregateFunc = require('../functions/aggregate');
